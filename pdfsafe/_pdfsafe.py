c55d4eb67e85d0955f6f064c4279af4c4c88cc1e
Finn Roy <finn.roy@stu.ocsb.ca>
2024-09-29 11:30:53 -0400

    Added aditional logging

--- pdfsafe/_pdfsafe.py
+++ pdfsafe/_pdfsafe.py
@@ -2,5 +2,8 @@ from pdf2image import convert_from_path
 
 
 def convert_to_safe(path: str) -> None:
+    print('Converting images')
     images = convert_from_path(path)
+    print('Saving')
     images[0].save(path, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:])
+    print('Done')
