ebd9ef8d560bc7a79e6fd111181d74df9a04ff0a
Finn Roy <finn.roy@stu.ocsb.ca>
2024-09-29 17:36:41 -0400

    Added trailing whitespace to overwrite previous log

--- pdfsafe/_pdfsafe.py
+++ pdfsafe/_pdfsafe.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import tempfile
 
 from pdf2image import convert_from_path
@@ -8,9 +10,17 @@ def convert_to_safe(path: str) -> None:
     images = []
     num_pages = PdfReader(open(path, "rb")).get_num_pages()
     
+    def log_action(action: str, idx: int, min_len: Optional[int] = 0) -> int:
+        log = "[Page %d/%d] %s %s" % (idx + 1, num_pages, action, path)
+        whitespace = ' ' * max(0, min_len - len(log))
+        
+        print(log + whitespace, end="\r")
+        
+        return len(log)
+    
     with tempfile.TemporaryDirectory() as temp:
         for idx in range(num_pages):
-            print("[Page %d/%d] Converting '%s'..." % (idx + 1, num_pages, path), end="\r")
+            min_len = log_action("Converting", idx)
             
             images += convert_from_path(
                 path, fmt="jpeg", dpi=100,
@@ -21,7 +31,7 @@ def convert_to_safe(path: str) -> None:
         pdf = PdfWriter()
         
         for idx, image in enumerate(images):
-            print("[Page %d/%d] Saving '%s'..." % (idx + 1, num_pages, path), end="\r")
+            log_action("Saving", idx, min_len)
             
             with tempfile.TemporaryFile() as temp_page:
                 image.save(temp_page, "PDF", resolution=100.0)
@@ -29,7 +39,7 @@ def convert_to_safe(path: str) -> None:
                 page = PdfReader(temp_page)
                 pdf.add_page(page.pages[0])
         
-        open(path, 'w').close() # completely erase pdf file
+        open(path, "w").close() # completely erase pdf file
         pdf.write(path) # write new data to file
         
         print() # newline
