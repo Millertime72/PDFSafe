d4d43bb57a0054285da928ed928743ba0ca6c98b
Finn Roy <finn.roy@stu.ocsb.ca>
2024-09-29 12:28:29 -0400

    Removed memory load, allowed processing

--- pdfsafe/__main__.py
+++ pdfsafe/__main__.py
@@ -15,7 +15,7 @@ def _normalize_path(path: str) -> str:
 
 def _get_pdf_paths(path: str) -> List[str]:
     if os.path.isdir(path):
-        return [os.path.join(root, file) for root, _, files in os.walk(path) for file in files if file.endswith('.pdf')]
+        return [file for file in os.listdir(path) if file.endswith('.pdf')]
     return [path]
 
 parser = ArgumentParser(
@@ -24,20 +24,13 @@ parser = ArgumentParser(
     epilog = "Not liable if your computer go boom."
 )
 
-parser.add_argument("path", help="path to pdf/directory containg pdf[s]")
-parser.add_argument("-r", "--recursive", action="store_true", help="convert all files under directory")
+parser.add_argument("path", help="path to pdf / directory containg pdf[s]")
 
 if __name__ == "__main__":
     args = parser.parse_args()
     
-    if not args.recursive and os.path.isdir(args.path):
-        parser.error("cannot convert '%s': is a directory" % args.path)
-    elif not os.path.exists(args.path):
+    if not os.path.exists(args.path):
         parser.error("connot convert '%s': does not exist" % args.path)
     
     for pdf in _get_pdf_paths(args.path):
-        print("Converting '%s'..." % pdf, end='')
-        
         convert_to_safe(pdf)
-        
-        print(" done.")
--- pdfsafe/_pdfsafe.py
+++ pdfsafe/_pdfsafe.py
@@ -1,9 +1,26 @@
-from pdf2image import convert_from_path
+import tempfile
 
+from pdf2image import convert_from_path
+from pypdf import PdfReader
+
+
+def _get_num_pages(path: str) -> int:
+    return PdfReader(open(path, "rb")).get_num_pages()
 
 def convert_to_safe(path: str) -> None:
-    print('Converting images')
-    images = convert_from_path(path)
-    print('Saving')
-    images[0].save(path, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:])
-    print('Done')
+    images = []
+    num_pages = _get_num_pages(path)
+    
+    with tempfile.TemporaryDirectory() as temp:
+        for idx in range(num_pages):
+            print("[Page %d/%d] Converting '%s'..." % (idx + 1, num_pages, path), end="\r")
+            
+            images += convert_from_path(
+                path, fmt="jpeg", dpi=100,
+                first_page=idx+1, last_page=idx+1,
+                output_folder=temp
+            )
+        
+        print()
+        
+        images[0].save(path, "PDF", resolution=100.0, save_all=True, append_images=images[1:])
